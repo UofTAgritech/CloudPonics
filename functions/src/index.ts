d42d17ff8eb24c000e948c694e3aa99a0afc64a6
Jayden Lefebvre <jayden.lefebvre55@gmail.com>
2021-11-09 15:00:01 -0500

    Restructured Cloud Functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,1 +1,6 @@
-export * from './device';
+import * as admin from 'firebase-admin';
+admin.initializeApp();
+
+export { deviceCreationListener, deviceDeletionListener } from './device/listeners/devicelist';
+export * from './device/listeners/pubsub';
+export * from './device/routes/registerDevice';
