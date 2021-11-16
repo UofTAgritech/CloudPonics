a8e5c5f479b684102a7fb0601f5b913248ed6add
Jayden Lefebvre <jayden.lefebvre55@gmail.com>
2021-11-16 03:52:16 -0500

    Changed Bucket Path Params
    Migrated from UID-based directory separation to CloudPonics project/run
    Added UID and device to custom metadata
    Moved storage interface object outside function call

--- functions/src/device/routes/uploadFile.ts
+++ functions/src/device/routes/uploadFile.ts
@@ -3,16 +3,27 @@ import * as functions from 'firebase-functions';
 import {Storage} from '@google-cloud/storage';
 
 type data = {
-  deviceName: string,
+  deviceId: string,
   filename: string,
+  project: string,
+  run: string,
   file: string | Buffer
 }
+
+const storage = new Storage();
 
 export const uploadFile = functions.https.onCall(async (data: data, context) => {
   if (!context.auth) {
     throw new functions.https.HttpsError('unauthenticated', 'You are not authenticated');
   }
-  const storage = new Storage();
-  const bucket = storage.bucket(`${context.auth.uid}/${data.deviceName}`).file(data.filename);
-  bucket.save(data.file);
+  
+  const bucket = storage.bucket(`projects/${data.project}/runs/${data.run}`).file(data.filename);
+  
+  return Promise.all([
+    bucket.setMetadata({
+      owner: context.auth.uid,
+      deviceId: data.deviceId
+    }),
+    bucket.save(data.file)
+  ]);
 });
