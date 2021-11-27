39104b76813b4036228dddb574d8497107c69f23
Jayden Lefebvre <jayden.lefebvre55@gmail.com>
2021-11-27 01:41:41 -0500

    Data Visualization

--- src/pages/Device.tsx
+++ src/pages/Device.tsx
@@ -1,7 +1,9 @@
 import React, {FC, useEffect, useState} from 'react'
 import {getFunctions, httpsCallable} from 'firebase/functions';
-import {getFirestore, doc, getDoc, onSnapshot, collection} from 'firebase/firestore';
-// import { useAuth } from '../contexts/AuthContext';
+import {getFirestore, doc, getDoc, onSnapshot, collection, query, orderBy, limit} from 'firebase/firestore';
+import {LineChart, XAxis, YAxis, Tooltip, CartesianGrid, Line} from 'recharts';
+import moment from 'moment';
+import { Typography } from '@material-ui/core';
 
 type DevicePageProps = {
   deviceid: string
@@ -31,30 +33,38 @@ const Device:FC<DevicePageProps> = (props) => {
     }[]
   }>({});
 
+  const formatTimestamps = (tick: number) => moment(tick).format('LTS');
+
   // Get datasets
   useEffect(() => {
     getDoc(doc(getFirestore(), `devices/${props.deviceid}`)).then(devicedoc=>{
-      console.log(devicedoc.data());
       setDevicedoc(devicedoc.data() as DeviceDoc);
       getDatasets({...devicedoc.data()?.latest}).then(datasets=>{
-        console.log(datasets);
         setDatasets(datasets.data);
       });
     });
   }, [])
 
+  const appendData = (dataset: string, newdata: {x: number, y: number}[], limit: number = 0) => {
+    setData((old)=>{
+      console.log(old);
+      return ({...old, [dataset]: (old[dataset] ?? []).concat(newdata).slice(-limit)})
+    });
+  }
   
   useEffect(() => {
-    let unsubs = datasets.map(dataset=>{
-      return onSnapshot(collection(getFirestore(), `projects/${devicedoc?.latest.project}/runs/${devicedoc?.latest.run}/${dataset}`), snapshot=>{
+    const unsubs = datasets.map(dataset=>{
+      return onSnapshot(query(collection(getFirestore(), `projects/${devicedoc?.latest.project}/runs/${devicedoc?.latest.run}/${dataset}`), orderBy('timestamp', 'desc'), limit(2)), snapshot=>{
+        let newdata: {x: number, y: number}[] = [];
         snapshot.docs.forEach(doc=>{
-          let newdata = {...data};
-          newdata[dataset] = (doc.data() as PubSubData).batch.map(datum=>{
+          newdata = newdata.concat((doc.data() as PubSubData).batch.map(datum=>{
             return {x: datum.timestamp, y: datum.value};
-          });
-          setData(newdata);
-          console.log(newdata);
+          }));
         })
+        newdata.sort((a, b)=>{
+          return a.x-b.x;
+        })
+        appendData(dataset, newdata, 100);
       });
     })
     // Unsub from all
@@ -65,9 +75,27 @@ const Device:FC<DevicePageProps> = (props) => {
   
 
   return (
-    <div>
-      {props.deviceid}
-    </div>
+    <>{Object.entries(data).map((dataset)=>(
+        <>
+          <Typography>
+            {dataset[0]}
+          </Typography>
+          <LineChart
+            key={'chart-'+dataset[0]}
+            width={1000}
+            height={400}
+            data={dataset[1]}
+            margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
+          >
+            <XAxis dataKey="x" tickFormatter={formatTimestamps}/>
+            <YAxis/>
+            <Tooltip />
+            <CartesianGrid stroke="#f5f5f5" />
+            <Line type="monotone" dataKey="y" stroke="#ff7300" yAxisId={0} />
+          </LineChart>
+        </>
+      )
+    )}</>
   )
 }
 
